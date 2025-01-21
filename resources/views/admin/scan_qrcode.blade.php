a2f72d7127ac8910ad7c0b4bc8ca7fbf0e17e9c0
tommypatra <tommyirawan.patra@gmail.com>
2025-01-21 11:12:00 +0800

    scan qrcode 2.8

--- resources/views/admin/scan_qrcode.blade.php
+++ resources/views/admin/scan_qrcode.blade.php
@@ -202,14 +202,17 @@
         // html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
 
 
-        const config = {
-            fps: 10,
-            qrbox: { width: 250, height: 250 },
-            formatsToSupport: [Html5QrcodeSupportedFormats.QR_CODE],
-            };
+        let config = {
+  fps: 10,
+  qrbox: {width: 100, height: 100},
+  rememberLastUsedCamera: true,
+  // Only support camera scan type.
+  supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA]
+};
 
-        const html5QrCode = new Html5Qrcode("reader");
-        html5QrCode.start({ facingMode: "environment" }, config, onScanSuccess);
+let html5QrcodeScanner = new Html5QrcodeScanner("reader", config, /* verbose= */ false);
+html5QrcodeScanner.render(onScanSuccess);
+
 
         $('#btnRefresh').click(function(){
             prosesDisposisi(vId);
